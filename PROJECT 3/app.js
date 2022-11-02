ed92ca8101011085e84d53e10a756e712c894163
kaladar <ckaladar@gmail.com>
2022-11-02 15:48:49 +0530

    Third project is completed

---
+++ PROJECT 3/app.js
@@ -0,0 +1,9 @@
+const circle = document.querySelector("#circle");
+
+circle.addEventListener("mouseenter", function () {
+  circle.classList.add("hover");
+});
+
+circle.addEventListener("mouseleave", function () {
+  circle.classList.remove("hover");
+});

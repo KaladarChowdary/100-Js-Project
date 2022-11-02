bce90d482921546e231b89dfbdbf0cb98964df3e
kaladar <ckaladar@gmail.com>
2022-11-02 17:49:46 +0530

    Part of project 4 is completed

---
+++ PROJECT 4/app.js
@@ -0,0 +1,5 @@
+const addToDoButton = document.getElementById("addToDo");
+const toDoContainer = document.getElementById("toDoContainer");
+const inputField = document.getElementById("inputField");
+
+console.log(addToDoButton, toDoContainer, inputField);

243c341af0e2a3ce52beef68a06e828c48aa92d7
kaladar <ckaladar@gmail.com>
2022-11-12 06:05:29 +0530

    7th project is completed

--- PROJECT 7/app.js
+++ PROJECT 7/app.js
@@ -1,4 +1,6 @@
 let counter = 1;
+const imgContainer = document.querySelector(".img-container");
+const buttons = document.querySelectorAll(".btn");
 
 function nextCounter() {
   counter = (counter % 8) + 1;
@@ -7,3 +9,16 @@ function nextCounter() {
 function prevCounter() {
   counter = counter === 0 ? 8 : counter - 1;
 }
+
+function setImageAccordingToCounter() {
+  imgContainer.style.background = `url('img/${counter}.jpg') center/contain fixed no-repeat`;
+}
+
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    if (btn.classList.contains("btn-left")) prevCounter();
+    if (btn.classList.contains("btn-right")) nextCounter();
+
+    setImageAccordingToCounter();
+  });
+});

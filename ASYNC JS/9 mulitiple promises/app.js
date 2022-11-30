78d21369c23e8b57aa8fd52d01d674e8bd693cfc
kaladar <ckaladar@gmail.com>
2022-11-30 06:25:25 +0530

    Created another async function
    Need to revise all these and get very very advanced

--- ASYNC JS/9 mulitiple promises/app.js
+++ ASYNC JS/9 mulitiple promises/app.js
@@ -1,12 +1,12 @@
 const promise1 = new Promise((resolve, reject) => {
   a = 1 + 1;
 
-  //   a = 3;
+  a = 3;
 
   if (a === 2) {
-    resolve("SuccessObject");
+    resolve("SuccessPromise1");
   } else {
-    reject("FailureObject");
+    reject("FailurePromise1");
   }
 });
 
@@ -16,28 +16,56 @@ const promise2 = new Promise((resolve, reject) => {
   //   a = 3;
 
   if (a === 2) {
-    resolve("SuccessObject");
+    resolve("SuccessPromise2");
   } else {
-    reject("FailureObject");
+    reject("FailurePromise2");
   }
 });
 
-promise1
-  .then(
-    (msg) => {
-      console.log(`Promise 1 : ${msg}`);
-      return promise2;
-    },
-    (msg) => {
-      console.log(`Promise 1 : ${msg}`);
-      return promise2;
-    }
-  )
-  .then(
-    (msg) => {
-      console.log(`Promise 2 : ${msg}`);
-    },
-    (msg) => {
-      console.log(`Promise 2 : ${msg}`);
-    }
-  );
+// promise1
+//   .then(
+//     (msg) => {
+//       console.log(`Promise 1 : ${msg}`);
+//       return promise2;
+//     },
+//     (msg) => {
+//       console.log(`Promise 1 : ${msg}`);
+//       return promise2;
+//     }
+//   )
+//   .then(
+//     (msg) => {
+//       console.log(`Promise 2 : ${msg}`);
+//     },
+//     (msg) => {
+//       console.log(`Promise 2 : ${msg}`);
+//     }
+//   );
+
+function pr1(msg) {
+  console.log(`Promise 1 : ${msg}`);
+  return promise2;
+}
+
+function pr2(msg) {
+  console.log(`Promise 2 : ${msg}`);
+}
+
+function pr(msg) {
+  console.log(`Promise Failed : ${msg}`);
+}
+
+// promise1.then(pr1).then(pr2).catch(pr);
+
+async function asyncCode() {
+  try {
+    const resParameter = await promise1;
+    pr1(resParameter);
+    const resParameter2 = await promise2;
+    pr2(resParameter2);
+  } catch (err) {
+    pr(err);
+  }
+}
+
+asyncCode();
